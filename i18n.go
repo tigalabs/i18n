0082fd72422401b5a7a2d8aaca017a9b5ccbdb1e
misitebao <i@misitebao.com>
2021-09-10 01:42:23 +0800

    first commit

---
+++ i18n.go
@@ -0,0 +1,6 @@
+package tgi18n
+
+// I18n 包自己的国际化
+type I18n struct {
+
+}
---
+++ tgi18n.go
@@ -0,0 +1,7 @@
+// Package tgi18n 是一个用于国际化的包
+package tgi18n
+
+// DefaultLanguage 默认语言
+var DefaultLanguage string = "en"
+
+
